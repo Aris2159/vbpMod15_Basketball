e7dabc3ab56a11507fa5654be55174990a5c985d
Aris <Aris2159@proton.me>
2022-12-16 11:45:16 -0400

    This program will display 2 plahyer names and then when you hit "Game action" it will display 2 players from diffrent team showing an action

--- vbpMod15_Basketball/Form1.Designer.vb
+++ vbpMod15_Basketball/Form1.Designer.vb
@@ -24,27 +24,34 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
-        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
         Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(105, 183)
+        Me.Button1.Location = New System.Drawing.Point(77, 132)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(174, 65)
         Me.Button1.TabIndex = 0
@@ -64,6 +71,42 @@ Partial Class Form1
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Team Blues"
         '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 73)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(88, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Patty Jones #21"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 58)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Player 2"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "John Smith #1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Player 1 "
+        '
         'GroupBox2
         '
         Me.GroupBox2.Controls.Add(Me.Label8)
@@ -77,59 +120,14 @@ Partial Class Form1
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Team Reds"
         '
-        'Label1
+        'Label8
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(6, 19)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(51, 15)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Player 1 "
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(6, 37)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(82, 15)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "John Smith #1"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(9, 58)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(48, 15)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Player 2"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(6, 73)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(88, 15)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Patty Jones #21"
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(6, 19)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(48, 15)
-        Me.Label5.TabIndex = 0
-        Me.Label5.Text = "Player 3"
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(6, 37)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(85, 15)
-        Me.Label6.TabIndex = 1
-        Me.Label6.Text = "Sheila Cook #5"
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(6, 73)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(85, 15)
+        Me.Label8.TabIndex = 3
+        Me.Label8.Text = "Brian Ning #14"
         '
         'Label7
         '
@@ -140,25 +138,70 @@ Partial Class Form1
         Me.Label7.TabIndex = 2
         Me.Label7.Text = "Player 4"
         '
-        'Label8
+        'Label6
         '
-        Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(6, 73)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(85, 15)
-        Me.Label8.TabIndex = 3
-        Me.Label8.Text = "Brian Ning #14"
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(5, 37)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(85, 15)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Sheila Cook #5"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 19)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 15)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Player 3"
         '
         'GroupBox3
         '
+        Me.GroupBox3.Controls.Add(Me.Label13)
+        Me.GroupBox3.Controls.Add(Me.Label12)
+        Me.GroupBox3.Controls.Add(Me.Label11)
         Me.GroupBox3.Controls.Add(Me.Label10)
-        Me.GroupBox3.Controls.Add(Me.Label9)
-        Me.GroupBox3.Location = New System.Drawing.Point(296, 23)
+        Me.GroupBox3.Location = New System.Drawing.Point(245, 23)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(167, 147)
+        Me.GroupBox3.Size = New System.Drawing.Size(173, 103)
         Me.GroupBox3.TabIndex = 3
         Me.GroupBox3.TabStop = False
-        Me.GroupBox3.Text = "Game"
+        Me.GroupBox3.Text = "Players Who are In the Court"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(6, 58)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(89, 15)
+        Me.Label13.TabIndex = 8
+        Me.Label13.Text = "From Red Team"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(6, 19)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(92, 15)
+        Me.Label12.TabIndex = 7
+        Me.Label12.Text = "From Blue Team"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(6, 73)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(0, 15)
+        Me.Label11.TabIndex = 6
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(6, 37)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(0, 15)
+        Me.Label10.TabIndex = 5
         '
         'Label9
         '
@@ -169,42 +212,64 @@ Partial Class Form1
         Me.Label9.TabIndex = 4
         Me.Label9.Text = "Game play"
         '
-        'Label10
-        '
-        Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(6, 37)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(47, 15)
-        Me.Label10.TabIndex = 5
-        Me.Label10.Text = "Label10"
-        '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(105, 254)
+        Me.Button2.Location = New System.Drawing.Point(251, 203)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(174, 65)
         Me.Button2.TabIndex = 4
         Me.Button2.Text = "Quit"
         Me.Button2.UseVisualStyleBackColor = True
         '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Label14)
+        Me.GroupBox4.Controls.Add(Me.Label9)
+        Me.GroupBox4.Location = New System.Drawing.Point(424, 23)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(203, 103)
+        Me.GroupBox4.TabIndex = 5
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Actions In Court"
+        '
+        'Label14
+        '
+        Me.Label14.Location = New System.Drawing.Point(6, 37)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(179, 51)
+        Me.Label14.TabIndex = 5
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(424, 132)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(174, 65)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Game Actions"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(672, 331)
+        Me.ClientSize = New System.Drawing.Size(634, 284)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox4)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Virtual Basket Ball"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox2.PerformLayout()
         Me.GroupBox3.ResumeLayout(False)
         Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -224,4 +289,10 @@ Partial Class Form1
     Friend WithEvents Label10 As Label
     Friend WithEvents Label9 As Label
     Friend WithEvents Button2 As Button
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label14 As Label
 End Class
--- vbpMod15_Basketball/Form1.vb
+++ vbpMod15_Basketball/Form1.vb
@@ -1,13 +1,74 @@
 ﻿'12/13/2022
+' This program will display 2 plahyer names and then when you hit "Game action" it will display 2 players from diffrent team showing an action
 
+Imports System.Reflection.Metadata.Ecma335
 
 Public Class Form1
+
+
+    'Button 1 block of code will select 1 player from Blue and Red team and display them to the court
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Rnd is a variable for Random
+        Dim rand As New Random()
+
+        ' Generate a random number between 1 and 2 which will help to choose a player to send in court
+        Dim num As Integer = rand.Next(1, 3)
+
+        ' Use the random number to index into the list of players and once the number have been selected then it will chose a player with that number
+        Dim players As New List(Of String) From {"John Smith, #1", "Patty Jones, #21"}
+        ' Tells that a player variable = One player which is selected from the 2 in above names
+        Dim player As String = players(num - 1)
+
+        ' Output the player's name
+        Console.WriteLine(player)
+        ' Display the player name on label 10 under "Player Who Are In Court"
+        Label10.Text = player
+
+        'Rnd is a variable for Random does the same thing but for the red team this time
+        Dim randnum As New Random()
+
+        ' Generate a random number between 1 and 2 which will help to choose a player to send in court
+        Dim number As Integer = randnum.Next(1, 3)
+
+        ' Use the random number to index into the list of players and once the number have been selected then it will chose a player with that number
+        Dim playersinredteam As New List(Of String) From {"Sheila Cook, #5", "Brian Ning, #14"}
+        ' Tells that a player variable = One player which is selected from the 2 in above names
+        Dim playerred As String = playersinredteam(num - 1)
+
+        ' Output the player's name
+        Console.WriteLine(playerred)
+        ' Display the player name on label 11 under "Player Who Are In Court"
+        Label11.Text = playerred
+
 
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         'End the program
         End
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Call Button1_Click(Label11.Text, Label10.Text)
+
+        Dim ranaction As New Random()
+
+        ' Generate a random number between 1 and 3
+        Dim number As Integer = ranaction.Next(1, 4)
+
+        ' Use the random number to index into an action it will have a nimber from 1 to 3 assign to every action listed below and select one out of 3
+        Dim actionproformed As New List(Of String) From {"dribble", "pass", "shoot"}
+        'tells that actionproformed is a variable and will select one action from 3 listed above and assign it with the variable "action"
+        Dim action As String = actionproformed(number - 1)
+
+        ' Output the players action
+        Console.WriteLine(action)
+        ' Display the action what is happning with 2 players and with string and spaces to put all that into a sentence
+        Label14.Text = (Label10.Text & Space(1) & "and" & Space(1) & Label11.Text & Space(1) & action & Space(1) & "the ball.")
+
+    End Sub
+
+    Private Sub Label12_Click(sender As Object, e As EventArgs) Handles Label12.Click
+
     End Sub
 End Class
